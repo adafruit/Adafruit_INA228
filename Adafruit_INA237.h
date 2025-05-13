d72faa9ce3a846f20ab4ff9b4f9a27c1981db736
ladyada <limor@ladyada.net>
2025-05-13 15:40:27 -0400

    fix 237 detect

--- Adafruit_INA237.h
+++ Adafruit_INA237.h
@@ -20,7 +20,7 @@
 #include "Adafruit_INA2xx.h"
 
 #define INA237_I2CADDR_DEFAULT 0x40 ///< INA237/INA238 default i2c address
-#define INA237_DEVICE_ID 0x237      ///< INA237 device ID
+#define INA237_DEVICE_ID 0x238      ///< INA237 device ID (technically doesnt have one)
 #define INA238_DEVICE_ID 0x238      ///< INA238 device ID
 
 /**
