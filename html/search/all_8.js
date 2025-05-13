8c4802dd3209d0e9ef6c1c25aca1ad3de5b715ca
Doxygen CI <ci-arduino@invalid>
2025-05-13 20:40:40 +0000

    Deploy docs to GitHub Pages from commit 73b9c91c78
    Commit: 73b9c91c7800c409ed164d1b0f8d028e0af42ec3
    GitHub Actions run: 15006229809

--- html/search/all_8.js
+++ html/search/all_8.js
@@ -7,7 +7,7 @@ var searchData=
   ['setaveragingcount',['setAveragingCount',['../class_adafruit___i_n_a2xx.html#a0392f9ab1cdc7b2336ad2b79db037769',1,'Adafruit_INA2xx']]],
   ['setcurrentconversiontime',['setCurrentConversionTime',['../class_adafruit___i_n_a2xx.html#af1548ef0e5d0909067d4d76b5f4a120a',1,'Adafruit_INA2xx']]],
   ['setmode',['setMode',['../class_adafruit___i_n_a2xx.html#ab8e0d2eb3399e0835a8b01c57bfa2970',1,'Adafruit_INA2xx']]],
-  ['setshunt',['setShunt',['../class_adafruit___i_n_a2xx.html#ac15f9a66950658c807fef8ffcc50cdf5',1,'Adafruit_INA2xx']]],
+  ['setshunt',['setShunt',['../class_adafruit___i_n_a228.html#ab7d1be4af28e9d3e2389deffcaaa6382',1,'Adafruit_INA228::setShunt()'],['../class_adafruit___i_n_a237.html#a6c17e64857065843706a46ccc80797a8',1,'Adafruit_INA237::setShunt()'],['../class_adafruit___i_n_a2xx.html#ac15f9a66950658c807fef8ffcc50cdf5',1,'Adafruit_INA2xx::setShunt()']]],
   ['settemperatureconversiontime',['setTemperatureConversionTime',['../class_adafruit___i_n_a2xx.html#a40bccdf92c63944b071f418827b2e7da',1,'Adafruit_INA2xx']]],
   ['setvoltageconversiontime',['setVoltageConversionTime',['../class_adafruit___i_n_a2xx.html#a718daa45ffee4d5b8601486dfdb35840',1,'Adafruit_INA2xx']]]
 ];
